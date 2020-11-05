322c7524e33f9301df6b1886793ce75f6c3ce943
paf <paf@titelfrei.de>
2020-11-05 16:26:01 +0100

    add test it('should not display pagination buttons when `pagingMode` = `concat-scroll`'

--- test/test.spec.js
+++ test/test.spec.js
@@ -30,7 +30,9 @@ describe('basic test', () => {
     });
 
     beforeEach(async done => {
-        await driver.get('file:' + playerPath);
+        if (!testConfig.keepOpen) {
+            await driver.get('file:' + playerPath);
+        }
         done();
     })
 
@@ -148,6 +150,28 @@ describe('basic test', () => {
         expect(await prevPage.isEnabled()).toBeFalse();
 
         done();
-    })
+    });
 
+    it('should not display pagination buttons when `pagingMode` = `concat-scroll`', async done => {
+        await send({
+            type: "vopStartCommand",
+            unitDefinition: "<fieldset><legend id='p1'>Page 1</legend></fieldset><fieldset><legend id='p2'>Page 2</legend></fieldset>",
+            sessionId: "1",
+            playerConfig: {
+                pagingMode: "concat-scroll"
+            }
+        });
+
+        const p1 = await driver.findElement(By.css('#p1'));
+        const p2 = await driver.findElement(By.css('#p2'));
+        const nextPage = await driver.findElement(By.css('#next-page'));
+        const prevPage = await driver.findElement(By.css('#prev-page'));
+
+        expect(await nextPage.isDisplayed()).toBeFalse();
+        expect(await prevPage.isDisplayed()).toBeFalse();
+
+        expect(await p1.isDisplayed()).toBeTrue();
+        expect(await p2.isDisplayed()).toBeTrue();
+        done();
+    });
 });
