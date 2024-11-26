25ccbf55fc236cb76ced21eef5a5b794b2c39e7b
paflov <philipp.franck@hu-berlin.de>
2024-11-26 15:10:44 +0100

    Testbed: Extend it to optionally use a host to test iframe specific stuff

--- dev-tools/testbed.js
+++ dev-tools/testbed.js
@@ -3,18 +3,21 @@
 const { Options } = require('selenium-webdriver/firefox');
 const { Builder } = require('selenium-webdriver');
 const fs = require('fs');
+const http = require('http');
 const config = require('./config.json');
 
 const path = fs.realpathSync(`${__dirname}/..`);
 
-const playerPath = `${path}/${config.player}`;
-const unitPath = `${path}/${config.unit}`;
-const unitDataPath = `${path}/${config.data}`;
-const playerConfigPath = `${path}/${config.playerConfig}`;
+const paths = {
+  player: `${path}/${config.player}`,
+  unit: `${path}/${config.unit}`,
+  unitData: `${path}/${config.data}`,
+  playerConfig: `${path}/${config.playerConfig}`
+};
 
 const sendStartCommand = async driver => {
-  const unitData = config.data ? JSON.parse(fs.readFileSync(unitDataPath, 'utf-8').toString()) : {};
-  const playerConfig = config.playerConfig ? JSON.parse(fs.readFileSync(playerConfigPath, 'utf-8').toString()) : {};
+  const unitData = config.data ? JSON.parse(fs.readFileSync(paths.unitData, 'utf-8').toString()) : {};
+  const playerConfig = config.playerConfig ? JSON.parse(fs.readFileSync(paths.playerConfig, 'utf-8').toString()) : {};
   const dataParts = Object.entries(unitData)
     .reduce((agg, entry) => {
       // eslint-disable-next-line no-param-reassign
@@ -23,7 +26,7 @@ const sendStartCommand = async driver => {
     }, {});
   await driver.executeScript(`window.postMessage(${JSON.stringify({
     type: 'vopStartCommand',
-    unitDefinition: fs.readFileSync(unitPath, 'utf-8').toString(),
+    unitDefinition: fs.readFileSync(paths.unit, 'utf-8').toString(),
     sessionId: '1',
     playerConfig,
     unitState: {
@@ -33,19 +36,41 @@ const sendStartCommand = async driver => {
   })})`);
 };
 
+const serve = () => {
+  http.createServer((req, res) => {
+    const filePath = paths[req.url.replace(/^\//, '')] || `${path}/${req.url}`;
+    console.log(`FETCH: ${filePath}`);
+    fs.readFile(filePath, (err, data) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.setHeader('Content-Security-Policy', '');
+      if (err) {
+        res.writeHead(500);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  })
+    .listen(9999);
+};
+
 (async () => {
   const options = new Options();
   const driver = await new Builder()
     .forBrowser('firefox')
     .setFirefoxOptions(options)
     .build();
-  await driver.get(`file:${playerPath}`);
-  await sendStartCommand(driver);
+  serve();
+  await driver.get(config.host ? `http://localhost:9999/${config.host}` : `file:${paths.player}`);
+  if (!config.host) await sendStartCommand(driver);
 
   fs.watch(path, { recursive: true }, async (eventType, filename) => {
     if (filename && filename.startsWith('.')) {
       await driver.navigate().refresh();
-      await sendStartCommand(driver);
+      if (!config.host) await sendStartCommand(driver);
     }
   });
 })();
